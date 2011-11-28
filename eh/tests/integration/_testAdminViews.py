eebc6235fb6656de48777f0c03780179f098ccf6
David McClure <david.mcclure@virginia.edu>
2011-11-28 17:31:57 -0500

    logout tests, broken

--- eh/tests/integration/_testAdminViews.py
+++ eh/tests/integration/_testAdminViews.py
@@ -127,14 +127,32 @@ class AdminLogoutTest(i.IntegrationTestCase):
     ''' /admin/logout '''
 
 
+    def testUserIdKeyPop(self):
+
+        ''' The logout route should pop the user_id key off of the session
+        hash, if there is one. '''
+
+        with self.app as c:
+
+            # Push in a user id.
+            with c.session_transaction() as s:
+                s['user_id'] = 1
+
+            # Check for the presence of the id.
+            self.assertEquals(session['user_id'], 1)
+
+            # Hit the logout route, confirm that the id is gone.
+            rv = c.get('admin/logout')
+            self.assertNotIn(session, 'user_id')
+
+
     def testRedirect(self):
 
-        ''' The logout route should always redirect to login. '''
+        ''' The logout route should redirect to login. '''
 
         # With no user_id in the session hash.
-        with self.app as c:
-            rv = c.get('admin/logout')
-            self.assertRedirect(rv, '/admin/login')
+        rv = self.app.get('admin/logout')
+        self.assertRedirect(rv, '/admin/login')
 
 
 if __name__ == '__main__':
